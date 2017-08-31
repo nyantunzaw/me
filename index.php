e920eaaf02c1302e84e63d4e7404976e43d85b10
nyantunzaw <nyantunzaw@gmail.com>
2017-09-01 01:54:51 +0800

    firebase

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 Welcome to my page
+<script src="https://www.gstatic.com/firebasejs/4.3.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB0i0ZPTgfNngf83lj0AOa75ZN7oo3xp2w",
+    authDomain: "nteezy-1.firebaseapp.com",
+    databaseURL: "https://nteezy-1.firebaseio.com",
+    projectId: "nteezy-1",
+    storageBucket: "nteezy-1.appspot.com",
+    messagingSenderId: "844277807485"
+  };
+  firebase.initializeApp(config);
+</script>
